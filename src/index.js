a819483d9cf7ac84747524fa6f43b7d40a7e10c7
artem <mr.artnov@mail.ru>
2022-08-06 11:50:26 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result="";
+    for (let i=0; i<expr.length ; i+=10){
+        let expr_first=expr.substr(i,10);
+        if (expr_first==='**********') result+=" ";
+        else {
+            let result_second="";
+            for (let i=0; i<expr_first.length; i+=2){
+                let expr_second=expr_first.substr(i,2);
+                if (expr_second==='00') {
+
+                }
+                else if (expr_second==='10') result_second+=".";
+                else if (expr_second==='11') result_second+='-';
+            }
+            result+=MORSE_TABLE[result_second]
+        }
+    }
+    return result;
+
 }
 
 module.exports = {
